5ab3592ce9582ebed3a59ae8c1f882a4323eb50c
HinnMachache <bennyhinn253.bh@gmail.com>
2024-09-02 22:26:14 +0300

    Collections Operations: Flattening, Filtering

--- udacity/src/collectionsOperations/Main.kt
+++ udacity/src/collectionsOperations/Main.kt
@@ -26,5 +26,20 @@ fun main() {
     println(zahlen.associateBy { it.first().uppercase() })
     println(zahlen.associateBy(keySelector = {it.first().uppercase()}, valueTransform = {it.length}))
 
+    // Flatten
+    val numberSet = listOf<Set<Int>>(setOf(1,2,3), setOf(4,5,6), setOf(7,8,9))
+//    for (numbers in numberSet) {
+//        for (number in numbers) {
+//            println(number)
+//        }
+//        println()
+//    }
+
+    val flattenedNumber = numberSet.flatten()
+//    for (number in flattenedNumber) {
+//        println(number)
+//    }
+    flattenedNumber.forEach { println(it) }
+
 }
 
